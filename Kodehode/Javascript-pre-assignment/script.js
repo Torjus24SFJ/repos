b4836b60a286aad3681909b196c9892b5f503f53
Torjus Tveten <Torjus24sfj@gmail.com>
2024-11-06 11:15:06 +0100

    something

---
+++ Kodehode/Javascript-pre-assignment/script.js
@@ -0,0 +1,44 @@
+// console.log(1 % 2);
+// console.log(2 % 2);
+// console.log(10%5);
+// console.log(68574%5);
+
+// console.log(5%2);
+
+// if (50%2 === 0) {
+//     console.log("noe")
+// }
+//     else {
+//         console.log("noe annet")
+//     }
+
+// const Hello = "Hello World!"
+
+// if (hour = 0) {
+//     return "invalid time" 
+// }
+// else if(hour >= 0 && hour >= 5) {
+    
+// }
+// else if(hour) {
+
+// }
+// else if(hour) {
+
+// }
+// else {
+//     return "invalid time"
+// }
+
+const colors = ["red", "green", "blue", "Yellow"]
+
+colors.pop()
+
+function removeFirstAndLastIndex() {
+
+}
+
+const something = "javascript is hard"
+console.log(something)
+
+    
