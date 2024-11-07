0faccd0ccd9426bf6ee1ee7b943e7f98d91ae469
Torjus Tveten <Torjus24sfj@gmail.com>
2024-11-07 15:32:42 +0100

    fetch

--- Kodehode/Javascript-api-fetch/Javascript/script.js
+++ Kodehode/Javascript-api-fetch/Javascript/script.js
@@ -1,24 +1,53 @@
 
-const content = document.getElementById("content")
-
-
+// const content = document.getElementById("content")
 
 //await makes sure the line is executed before running next line
 //await must be within async
 //fetching api link with data like title and images
-async function getData() {
-    const response = await fetch("https://ghibliapi.vercel.app/films")
-    const data = await response.json()
-    console.log(data)
 
-    content.innerHTML = (data.map((movie) => {
+//async function getData() {
+//     const response = await fetch("https://ghibliapi.vercel.app/films")
+//     const data = await response.json()
+//     console.log(data)
+
+//     content.innerHTML = (data.map((movie) => {
+//         return (
+//         `
+//         <div class="movie-card"> 
+//         <h2>${movie.title}</h2>
+//         <h5>${movie.original_title}</h5>
+//         <img src="${movie.image}" />
+//         <h5 id="year">${movie.release_date}</h5>
+
+//         </div>
+//         `
+//         )
+//     }).join(""))
+
+// };
+// getData();
+
+const content = document.getElementById("content")
+
+async function getData() {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=386")
+    const data = await response.json()
+    // console.log(data)
+
+
+    const pokemonUrls = data.results.map((pokemon) => pokemon.url)
+    const pokemonPromises = pokemonUrls.map((url) => fetch(url).then((response) => response.json()))
+    const pokeDetails = await Promise.all(pokemonPromises);
+
+    console.log(pokeDetails)
+
+
+    content.innerHTML = (pokeDetails.map((pokemon) => {
         return (
         `
-        <div class="movie-card"> 
-        <h2>${movie.title}</h2>
-        <h5>${movie.original_title}</h5>
-        <img src="${movie.image}" />
-
+        <div class="pokemon-card"> 
+            <h2>${pokemon.name}</h2>
+            <img src="${pokemon.sprites.front_default}"/>
         </div>
         `
         )
