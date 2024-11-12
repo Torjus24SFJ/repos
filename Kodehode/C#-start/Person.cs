1e8fdca061208b8d698c9f38b3ccda29efa33223
Torjus Tveten <Torjus24sfj@gmail.com>
2024-11-12 09:56:48 +0100

    setup new cli project

---
+++ Kodehode/C#-start/Person.cs
@@ -0,0 +1,12 @@
+class Person {
+    //Data fields
+    public string firstName;
+    public string lastName;
+
+//Contructor, defines the logic for creating this object
+    public Person(string firstName, string lastName)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
---
+++ Kodehode/C#-start/Program.cs
@@ -0,0 +1,69 @@
+﻿// string testing = "tester";
+// Console.WriteLine(testing);
+
+// bool isEnabled = true;
+// if (isEnabled) {
+//     Console.WriteLine("IS TRUE");
+// };
+
+
+// Console.WriteLine("Hello");
+
+// int count = 0;
+// while (count < 10) {
+//     Console.WriteLine("Count: " + count);
+//     count++;
+// }
+
+// Person lars = new Person("Lars", "Monsen");
+// Console.WriteLine("The persons first name is: " + lars.firstName);
+// Console.WriteLine("The persons last name is: " + lars.lastName);
+
+
+// int numberOfPersons = 0;
+// while (numberOfPersons < 6)
+// {
+//     Person person = new Person("player", "Annon" + numberOfPersons);
+//     Console.WriteLine("Player: " + person.lastName);
+//     numberOfPersons++;
+// }
+
+//Loops 
+// int i = 1;
+//  do
+//  {
+//   Console.WriteLine(i);
+//   i++;
+// }
+// while (i < 6);
+
+//While loops
+// int i = 1;
+// while (i<6){
+//     Console.WriteLine(i);
+//     i++;
+// }
+
+//For loops
+// for (int i = 0; i < 5; i++){
+//     Console.WriteLine("Yes");
+// }
+
+
+//Array
+// string[] cars = {"BMW", "Tesla", "SUZUKI"};
+// Console.WriteLine(cars[1]);
+
+// string[] cars = {"BMW", "Tesla", "SUZUKI", "MAZDA", "Jeep"};
+// Array.Sort(cars);
+// foreach(string i in cars){
+//     Console.WriteLine(i);
+// }
+
+//Ternary operation (Short hand expression) ? Expression true : Expression false
+// int x = 10;
+// int y = 9;
+
+// string result = (x > y) ? "True" : "False";
+// Console.WriteLine(result);
+
