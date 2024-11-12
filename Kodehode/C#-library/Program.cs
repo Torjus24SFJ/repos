d95b4d5630fd0592997706762b943faac3114bcd
Torjus Tveten <Torjus24sfj@gmail.com>
2024-11-12 10:31:30 +0100

    user input parsing

--- Kodehode/C#-library/Program.cs
+++ Kodehode/C#-library/Program.cs
@@ -1,5 +1,36 @@
-﻿// les av bruker input
-Console.WriteLine("Hello");
-string userInput = Console.ReadLine();
+﻿// Les av bruker input
+
+// Console.WriteLine("Write something: ");
+// string? userInput = Console.ReadLine();
 
 // Ekko tilbake det bruker skriver
+
+// Console.WriteLine("Echo " + userInput);
+
+
+
+
+bool runProgram = true;
+while (runProgram)
+{
+    // Find out what userinput is
+    Console.WriteLine("Do you want to lend or return?");
+    string? userInput = Console.ReadLine();
+
+    // Lend out a book
+    if (userInput == "lend")
+    {
+        Console.WriteLine("Lending a book");
+    }
+    // Return book
+    else if (userInput == "return")
+    {
+        Console.WriteLine("Returning a book");
+    }
+    else if (userInput == "exit")
+    {
+        runProgram = false;
+    }
+}
+
+
