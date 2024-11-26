02c028c92a3a7cbb80663bc467daaebef3767d49
Torjus Tveten <Torjus24sfj@gmail.com>
2024-11-26 14:49:14 +0100

    Finished nettside

--- Kodehode/Nettside-oppgave/script.js
+++ Kodehode/Nettside-oppgave/script.js
@@ -1,21 +1,2 @@
+// I tried...
 
-function randomImg(){
-    document.getElementById("random-img").addEventListener('click', function(event){
-        event.preventDefault();
-        
-        const pictureArr = [
-            "./images/airbnb-cat.PNG",
-            "./images/foreignreleations-cat.PNG",
-            "./images/pestcontrol-cat.PNG",
-            "./images/primeminister-cat.PNG",
-            "./images/security-cat.PNG",
-            "./images/shopsupervisor-cat.PNG",
-            "./images/spy-cat.PNG",
-        ];
-
-        const mappedArr = pictureArr.map(img => img);
-        const randomIndex = Math.floor(Math.random() * mappedArr.length);
-        const randomImg = mappedArr[randomIndex];
-        window.location.href = `random.html?image=${encodeURIComponent(randomImg)}`;
-    });
-}
